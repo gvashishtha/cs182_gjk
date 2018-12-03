452aade56ea71ec59f445401252d5c51c83eb5a8
Gopal K. Vashishtha <gopalvashishtha@college.harvard.edu>
2018-12-03 08:26:01 -0500

    bug fixes, simulated annealing

--- util.py
+++ util.py
@@ -1,10 +1,14 @@
 import copy
+import math
+import random
 
 class Constraint(object):
     """
     Constrains a single object of class Variable. (see p. 84 of Ovans)
 
     """
+    def __repr__(self):
+        return 'Constraint object with var {}'.format(self.var)
 
     def setVariable(self, var):
         self.var = var
@@ -21,7 +25,7 @@ class Constraint(object):
         for x in domain1:
             satisfied = False
             for y in domain2:
-                if predicate(x, y):
+                if predicate(y, x):
                     satisfied = True
                     break
             if not satisfied:
@@ -41,6 +45,9 @@ class Constraint(object):
             return True
 
 class Constraint3(Constraint):
+    def __repr__(self):
+        return 'Constraint3 object with var {}, var2 {}'.format(self.var, self.var2)
+
     def setVariable2(self, var):
         self.var2 = var
 
@@ -167,7 +174,7 @@ class Variable(object):
         return result
 
     def instantiate(self, obj):
-        #new_var = Variable(self.name + 'child')
+        # Test whether a given instantiation will work
         temp_domain = copy.deepcopy(self.domain)
         self.domain = [obj]
         if self.alertChanged():
@@ -179,8 +186,9 @@ class Variable(object):
 class Csp(object):
     def __init__(self):
         self.vars = []
-        self.bts = self.nodes = self.sol = self.count = 0
+        self.bts = self.nodes = self.sol = self.iters = 0
         self.one_sol = None
+        self.initial_domains = {}
 
     def addToVariables(self, var):
         self.vars.append(var)
@@ -196,18 +204,17 @@ class Csp(object):
         cur_var = self.vars[index]
         cur_domain = copy.deepcopy(cur_var.getDomain())
         index += 1
-
         success = False
         for i in range(len(cur_domain)):
             saved_vars = {}
             for j in range(index, numVariables):
-                saved_vars[j] = copy.deepcopy(self.vars[j])
+                saved_vars[j] = copy.deepcopy(self.vars[j].getDomain())
             if cur_var.instantiate(cur_domain[i]):
                 success = True
                 self.nodes += 1
                 self.findSolutions2(index, numVariables)
             for j in range(index, numVariables):
-                self.vars[j] = saved_vars[j]
+                self.vars[j].setDomain(saved_vars[j])
         if not success:
             self.bts += 1
 
@@ -217,10 +224,8 @@ class Csp(object):
         return self.findSolutions2(0, len(self.vars))
 
     def findOneSolution(self, index, numVariables):
-        #print('finding solution at index {} with numVariables {}'.format(index, numVariables))
         if index == numVariables:
             self.sol += 1
-            # print('solution is {}'.format(self.vars))
             self.one_sol = copy.deepcopy(self.vars)
             return True
 
@@ -234,14 +239,14 @@ class Csp(object):
         for i in range(len(cur_domain)):
             saved_vars = {}
             for j in range(index, numVariables):
-                saved_vars[j] = (copy.deepcopy(self.vars[j]))
+                saved_vars[j] = copy.deepcopy(self.vars[j].getDomain())
             if cur_var.instantiate(cur_domain[i]):
                 self.nodes += 1
                 success = self.findOneSolution(index, numVariables)
 
             #if not success:
             for j in range(index, numVariables):
-                self.vars[j] = saved_vars[j]
+                self.vars[j].setDomain(saved_vars[j])
             if success:
                 break
 
@@ -271,3 +276,50 @@ class Csp(object):
                 print('failing variable is {}'.format(self.vars[i]))
                 return False
         return True
+
+    def getCost(self, vars): # Should return a cost heuristic representing distance from a satisfying assignment
+        cost = 0
+        for var in vars:
+            member1 = var.getDomain()[0]
+            for n in var.neighbors:
+                constraint = n.getNode()
+                member2 = constraint.var.getDomain()[0]
+                try: # look for 3 constraints
+                    member3 = constraint.var2.getDomain()[0]
+                    if not n.label(member1, member2, member3):
+                        cost += 1
+                except AttributeError:
+                    if not n.label(member1, member2):
+                        cost += 1
+        return cost
+
+    def getNeighbor(self, vars):
+        to_change = random.choice(range(len(vars)))
+        new_val = random.choice(self.initial_domains[to_change])
+        vars[to_change].setDomain([new_val])
+
+    def simAnnealing(self):
+        # generate random initialization
+        T = 5000
+        for i in range(len(self.vars)):
+            self.initial_domains[i] = copy.deepcopy(self.vars[i].getDomain())
+            self.vars[i].setDomain([random.choice(self.initial_domains[i])])
+        cur_cost = self.getCost(self.vars)
+        while self.getCost(self.vars) != 0 and T > 1:
+            T -= 1
+            self.iters += 1
+            # save current assignments
+            saved_vars = {}
+            for i in range(len(self.vars)):
+                saved_vars[i] = copy.deepcopy(self.vars[i].getDomain())
+
+            new_assignment = self.getNeighbor(self.vars)
+
+            # From https://en.wikipedia.org/wiki/Simulated_annealing#Acceptance_probabilities
+            accept_prob = math.exp(-(float(self.getCost(self.vars))-float(cur_cost)/float(T)))
+            if self.getCost(self.vars) > cur_cost and random.random() > accept_prob:
+                for i in range(len(self.vars)):
+                    self.vars[i].setDomain(saved_vars[i])
+                continue
+            else:
+                cur_cost = self.getCost(self.vars)
