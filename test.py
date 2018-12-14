70ed140da4d2ae38f2a334f58712c8ca60654113
Jake <jsteeves@college.harvard.edu>
2018-12-14 16:56:38 -0500

    Create test data csvs and options for main program

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from main import main as generate_music
+from optparse import Values
 import os
 import timeit
 
@@ -13,17 +14,37 @@ test_dir = 'tests/'
 if not os.path.exists(test_dir):
     os.makedirs(test_dir)
 
+# Insert column headers for dfs trials info
+with open('dfs_trial_info.csv', 'w+') as f:
+    f.write('bars,nodes expanded,backtracks,runtime\n')
+f.closed
+
+# Insert column headers for simulated annealing trials info
+with open('simulated_annealing_trial_info.csv', 'w+') as f:
+    f.write('bars,cost,iterations,runtime\n')
+f.closed
+
 def file_name_generator(type, num_bars, trial_num):
     return type + str(num_bars) + '_bars_trial_' + str(trial_num) + '.mid'
 
 # Generate composition with specified parameters. Returns runtime in seconds
 def test_generation(num_bars, cantus_file, solution_file, sa_file, test_dir):
-  start = timeit.default_timer()
-  returned_csp = generate_music(num_bars=num_bars, cantus_file=cantus_file, solution_file=solution_file, sa_file=sa_file, test_dir=test_dir, testing=True)
-  if returned_csp is not None:
-      assert(returned_csp.getCost(returned_csp.one_sol)==0)
-  stop = timeit.default_timer()
-  return stop - start
+    options = Values()
+    options.preset_song = ''
+    options.num_bars = num_bars
+    options.cantus_file = cantus_file
+    options.solution_file = solution_file
+    options.sa_file = sa_file
+    options.test_dir = test_dir
+    options.testing = True
+    options.arc_consistency = True
+
+    start = timeit.default_timer()
+    returned_csp = generate_music(options=options)
+    if returned_csp is not None:
+        assert(returned_csp.getCost(returned_csp.one_sol)==0)
+    stop = timeit.default_timer()
+    return stop - start
 
 print('Bars range:' + str(bars_range))
 print(str(num_trials) + ' trials')
